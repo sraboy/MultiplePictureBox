ca00483a44ad5b62572e40de59124476950a43d5
Steve Lavoie <steven.lavoiejr@gmail.com>
2015-06-06 20:40:00 -1000

    initial commit
    Signed-off-by: Steve Lavoie <steven.lavoiejr@gmail.com>

---
+++ MultiplePictureBox/MultiplePictureBox.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace MultiplePictureBox
+{
+    partial class MultiplePictureBox
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ImageContainer = new System.Windows.Forms.PictureBox();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageContainer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ImageContainer
+            // 
+            this.ImageContainer.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ImageContainer.Location = new System.Drawing.Point(3, 3);
+            this.ImageContainer.Name = "ImageContainer";
+            this.ImageContainer.Size = new System.Drawing.Size(181, 204);
+            this.ImageContainer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ImageContainer.TabIndex = 0;
+            this.ImageContainer.TabStop = false;
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnPrevious.Location = new System.Drawing.Point(6, 213);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(75, 23);
+            this.btnPrevious.TabIndex = 1;
+            this.btnPrevious.Text = "Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnNext.Location = new System.Drawing.Point(105, 213);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // MultiplePictureBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.ImageContainer);
+            this.Name = "MultiplePictureBox";
+            this.Size = new System.Drawing.Size(187, 240);
+            ((System.ComponentModel.ISupportInitialize)(this.ImageContainer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ImageContainer;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+    }
+}
---
+++ MultiplePictureBox/MultiplePictureBox.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MultiplePictureBox
+{
+    public partial class MultiplePictureBox : UserControl
+    {
+        private int imagecount;
+        private int displayimage = 0;
+        public Image[] images;
+
+        public int ImageCount
+        {
+            get
+            {
+                return this.imagecount;
+            }
+            set
+            {
+                this.imagecount = value;
+            }
+        }
+
+        public MultiplePictureBox()
+        {
+            InitializeComponent();
+        }
+
+        public void Initialize(string[] filenames)
+        {
+            ImageCount = filenames.Count();
+            images = new Image[ImageCount];
+
+            for (int x = 0; x < imagecount; x++)
+            {
+                images[x] = Image.FromFile(filenames[x]);
+            }
+        }
+
+        #region AddImage() - DEPRECATED
+        /// <summary>
+        /// DEPRECATED - Adds another image to the array
+        /// </summary>
+        /// <param name="image">The Image object to add to the array</param>
+        public bool AddImage(Image image)
+        {
+            int x = 0;
+
+            //Cycles through the array to find an empty slot for the new image
+            while (images[x] != null)
+            {
+                if (x < imagecount)
+                {
+                    x++;
+                    if (images[x] == null)
+                    {
+                        images[x] = image;
+                        return true;
+                    }
+                }
+                else
+                    return false;
+            }
+
+            images[x] = image;
+            return true;
+        }
+        #endregion
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (displayimage < imagecount)
+            {
+                displayimage++;
+                if (displayimage == imagecount)
+                    displayimage = 0;
+
+
+                ImageContainer.Image = images[displayimage];
+            }
+            else
+                displayimage = 0;
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+                if (displayimage > 0)
+                {
+                    displayimage--;
+                    if (displayimage == -1)
+                        displayimage = imagecount;
+
+                    ImageContainer.Image = images[displayimage];
+                }
+                else if (displayimage == 0)
+                {
+                    displayimage = (imagecount - 1);
+                    ImageContainer.Image = images[displayimage];
+                }
+        }
+    }
+}
